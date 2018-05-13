26b2e7aedc51070974e3c4f37affc0867fcec57f
raviteja.kamalapuram <raviteja.kamalapuram@gmail.com>
2018-05-14 00:47:21 +0530

    api moved to www

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const fs = require('fs');
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-var obj1 = require('./API/CONTENTLISTINGPAGE-PAGE1');
-var obj2 = require('./API/CONTENTLISTINGPAGE-PAGE2');
-var obj3 = require('./API/CONTENTLISTINGPAGE-PAGE3');
+var obj1 = require('./www/API/CONTENTLISTINGPAGE-PAGE1');
+var obj2 = require('./www/API/CONTENTLISTINGPAGE-PAGE2');
+var obj3 = require('./www/API/CONTENTLISTINGPAGE-PAGE3');
 var movies = [];
 
 obj1['page']['content-items']['content'].map(o => {
